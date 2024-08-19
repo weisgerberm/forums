d4cb5fbca4bba9e97a28221862e07a9ca7d2dcf6
Mark Weisgerber <m.weisgerber@datamints.com>
2024-08-19 23:59:03 +0200

    prepared the forums module

--- Classes/Controller/BackendController.php
+++ Classes/Controller/BackendController.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Weisgerber\Forums\Controller;
 
+use GeorgRinger\News\Domain\Repository\NewsRepository;
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
 use Weisgerber\DarfIchMit\Controller\AbstractBackendController;
 use Weisgerber\DarfIchMit\Traits\ModuleTemplateServiceTrait;
 
@@ -26,6 +29,15 @@ class BackendController extends AbstractBackendController
      */
     public function newsAction(): ResponseInterface
     {
+        /** @var NewsRepository $newsRepository */
+        $newsRepository = GeneralUtility::makeInstance(NewsRepository::class);
+        /** @var Typo3QuerySettings $querySettings */
+        $querySettings = GeneralUtility::makeInstance(Typo3QuerySettings::class);
+        $querySettings->setRespectStoragePage(false);
+        $newsRepository->setDefaultQuerySettings($querySettings);
+        $news = $newsRepository->findAll();
+
+        $this->view->assign('records', $news);
 
         return $this->htmlResponse($this->createBackendView());
     }
