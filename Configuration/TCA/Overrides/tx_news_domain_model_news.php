3cd733a687a0f6315c9ce82dedf6f695e0722915
Mark Weisgerber <m.weisgerber@datamints.com>
2024-08-23 21:44:17 +0200

    add news field

---
+++ Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -0,0 +1,22 @@
+<?php
+
+use Weisgerber\DarfIchMit\Utility\TcaUtility;
+
+defined('TYPO3') or die();
+
+$tmp_news_columns = [
+    'forums_thread' => [
+        'exclude' => true,
+        'label' => TcaUtility::title('thread'),
+        'config' => [
+            'type' => 'inline',
+            'foreign_table' => \Weisgerber\Forums\Domain\Model\Thread::TABLE_NAME,
+            'maxitems' => 1,
+            'appearance' => [
+                'collapseAll' => 1,
+                'levelLinksPosition' => 'none',
+            ],
+        ],
+    ],
+];
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_news_domain_model_news', $tmp_news_columns);
